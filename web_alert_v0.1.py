083136833aa3782eac2e72378962d149e0b3b437
José Fierro <46327577+sevid42@users.noreply.github.com>
2021-03-26 21:14:15 -0300

    Add files via upload

---
+++ web_alert_v0.1.py
@@ -0,0 +1,56 @@
+import cv2
+from time import sleep
+import datetime
+import os
+  
+lista_estados=[None,None]
+imagen_inicial=None
+video=cv2.VideoCapture(0)
+
+while True:
+    check, frame = video.read()
+    estado=0
+    gray_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray_frame=cv2.GaussianBlur(gray_frame,(25,25),2) #el ultimo es la desviacion, a mas grandes mas borroso
+
+    if imagen_inicial is None:
+        imagen_inicial=gray_frame
+        continue
+
+    delta=cv2.absdiff(imagen_inicial,gray_frame)
+    threshold=cv2.threshold(delta, 30, 255, cv2.THRESH_BINARY)[1]
+    (contornos,_)=cv2.findContours(threshold,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2:]
+
+    for contorno in contornos:
+        if cv2.contourArea(contorno) < 10000:
+            continue
+        estado=1
+        (x, y, w, h)=cv2.boundingRect(contorno)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 1)
+    lista_estados.append(estado)
+
+    if lista_estados[-1]==1 and lista_estados[-2]==0:
+        #t = threading.Thread(target=thread_voice_alert, args=(engine,))
+        #t.start()
+        print("Te pillamos master")
+        
+
+        name=str(datetime.datetime.now())
+        name = name.replace(":", "")
+        name = name.replace(".", "_")+ '.jpg'
+        
+        cv2.imwrite(name,frame)
+        sleep(0.1)
+  
+
+    cv2.imshow("gray_frame Frame",gray_frame)
+    cv2.imshow("Delta Frame",delta)
+    cv2.imshow("Threshold Frame",threshold)
+    cv2.imshow("Color Frame",frame)
+    
+    if cv2.waitKey(1)==ord('q'):
+        break
+
+#libera recursos
+video.release()
+cv2.destroyAllWindows
